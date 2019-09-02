0cf86bc3c7aa3ea3627787285387673a888d89bb
S-Hanin <sign.rx@yandex.ru>
2019-09-02 12:32:18 +0300

    change scripts install path

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     description='Declarative xml mapping library',
     install_requires=['lxml', 'python-dateutil'],
     long_description=open(join(dirname(__file__), 'README.md')).read(),
-    scripts=['bin/xml2class.py', 'bin/xml2class_bulk.py']
+    scripts=['PyXmlMapper/xml2class.py', 'PyXmlMapper/xml2class_bulk.py']
 )
